35c2e965982e16521e84435a0a9dca0f0743745e
cgcladera <cgcladera@gmail.com>
2014-10-29 17:49:53 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# videojs-offset
+
+VideoJS plugin to play a segment of a video
+
+## About
+
+A JavaScript library by Carles Galan Cladera.
+
+See the [project homepage](http://cladera.github.io/videojs-offset).
+
+## Installation
+
+Using Bower:
+
+    bower install videojs-offset
+
+Or grab the [source](https://github.com/cladera/videojs-offset/dist/videojs-offset.js) ([minified](https://github.com/cladera/videojs-offset/dist/videojs-offset.min.js)).
+
+## Usage
+
+Basic usage is as follows:
+
+    videojs-offset();
+
+For advanced usage, see the documentation.
+
+## Documentation
+
+Start with `docs/MAIN.md`.
+
+## Contributing
+
+We'll check out your contribution if you:
+
+* Provide a comprehensive suite of tests for your fork.
+* Have a clear and documented rationale for your changes.
+* Package these up in a pull request.
+
+We'll do our best to help you out with any contribution issues you may have.
+
+## License
+
+MIT. See `LICENSE.txt` in this directory.
---
+++ src/main.js
@@ -0,0 +1,63 @@
+/* videojs-offset main */
+
+// Base function.
+var vjsoffset = function(options) {
+  var Player, start, end;
+  Player = this.constructor;
+  Player.__super__ = {
+    duration: Player.prototype.duration,
+    currentTime: Player.prototype.currentTime,
+    bufferedPercent: Player.prototype.bufferedPercent,
+    remainingTime: Player.prototype.remainingTime
+  };
+  start = options.start || 0;
+  end = options.end || 0;
+
+  Player.prototype.duration = function(){
+    if(end > 0) {
+      return end - start;
+    }
+    return Player.__super__.duration.apply(this, arguments) - start;
+  };
+
+  Player.prototype.currentTime = function(seconds){
+    if(seconds !== undefined){
+      return Player.__super__.currentTime.call(this, seconds + start) - start;
+    }
+    return Player.__super__.currentTime.apply(this, arguments) - start;
+  };
+
+  Player.prototype.remainingTime = function(){
+    var curr = this.currentTime();
+    if(curr < start) {
+      curr = 0;
+    }
+    return this.duration() - curr;
+  };
+  this.on('play', function(){
+
+  });
+  this.on('timeupdate', function(){
+    var curr = this.currentTime();
+    if(curr < 0){
+      this.currentTime(0);
+      this.play();
+    }
+    if(curr > (end-start)) {
+      this.currentTime(end-start);
+      this.pause();
+    }
+  });
+
+  return this;
+
+};
+
+
+// Version.
+vjsoffset.VERSION = '0.0.0';
+
+
+// Export to the root, which is probably `window`.
+window.vjsoffset = vjsoffset;
+window.videojs.plugin('offset', vjsoffset);
