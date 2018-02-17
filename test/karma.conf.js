f521253aa74bb43dc20618c891639da1fff5ec79
Carles Galan Cladera <cgcladera@gmail.com>
2018-02-17 11:38:26 +0100

    fix: string inputs
    Parse start and end options to float.
    
    Fixes #25

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,26 +1,21 @@
 module.exports = function(config) {
-  var detectBrowsers = {
-    enabled: false,
-    usePhantomJS: false
-  };
-
   // On Travis CI, we can only run in Firefox and Chrome; so, enforce that.
   if (process.env.TRAVIS) {
     config.browsers = ['Firefox', 'travisChrome'];
   }
 
-  // If no browsers are specified, we enable `karma-detect-browsers`
-  // this will detect all browsers that are available for testing
+  // If no browsers are specified, assume is dev env and run ChromeHeadless
+  // TODO: re-enable detect browsers when figure out how to make test work in
+  // Safari
   if (!config.browsers.length) {
-    detectBrowsers.enabled = true;
+    config.browsers = ['ChromeHeadless']
   }
 
   config.set({
     basePath: '..',
-    frameworks: ['qunit', 'detectBrowsers'],
+    frameworks: ['qunit'],
     files: [
       'node_modules/video.js/dist/video-js.css',
-      
       'node_modules/es5-shim/es5-shim.js',
       'node_modules/sinon/pkg/sinon.js',
       'node_modules/video.js/dist/video.js',
@@ -32,7 +27,6 @@ module.exports = function(config) {
         flags: ['--no-sandbox']
       }
     },
-    detectBrowsers: detectBrowsers,
     reporters: ['dots'],
     port: 9876,
     colors: true,
